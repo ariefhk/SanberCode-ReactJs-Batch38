2d35e7942925dadfd1b3ea9ef4470a5138090964
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-09-21 17:38:38 +0700

    replace all with js

---
+++ sanbercode-progress/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Introduction from "./components/Introduction";
+
+function App() {
+  return (
+    <div className="container">
+      <Introduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ sanbercode-progress/src/components/Introduction/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../style/introduction.css";
+
+function index() {
+  return (
+    <div className="card">
+      <h1>Data Peserta Sanbercode Bootcamp Reactjs</h1>
+      <ol>
+        <li>
+          <strong>Nama:</strong> Arief Rachman Hakim
+        </li>
+        <li>
+          <strong>Email:</strong> arierachmanhakim1001@gmail.com
+        </li>
+        <li>
+          <strong>Sistem Operasi yang digunakan:</strong> Windows
+        </li>
+        <li>
+          <strong>Akun Github:</strong> ariefhk
+        </li>
+        <li>
+          <strong>Akun Telegram:</strong> Ariefrhk
+        </li>
+        <li>
+          <strong>Tanggal lahir:</strong> 22-05-2002
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default index;
